69d834df764cb8be74fbb79cba5959cdefddc5a3
Jakub Matys <jakub.matys@nodus.cz>
2015-11-08 17:41:25 +0100

    Copyright

--- main_test.go
+++ main_test.go
@@ -1,4 +1,4 @@
-// Copyright 2015 Jakub Matys
+// Copyright 2014-2015 Jakub Matys
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
